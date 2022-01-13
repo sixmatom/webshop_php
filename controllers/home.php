9976074eed73c74676e25f579d044b90e6d3ca46
Tom Sixma <sixmatom@gmail.com>
2022-01-13 13:52:16 +0100

    data from php instead of js

--- controllers/home.php
+++ controllers/home.php
@@ -2,13 +2,15 @@
 
 function index($view)
 {
+    $screens = require 'data/screens.php';
+
     return require $_SERVER['DOCUMENT_ROOT'] . '/assets/views/' . $view . '.view.php';
 }
 
 function saveCard()
 {
     dd($_REQUEST);
-    
+
     echo json_encode([
         'success'   => true,
         'message'   => 'Fruit added to order',
