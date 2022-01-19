9bbd40772f73bb4dc35b4873eeda3ae9787346ba
Tom Sixma <sixmatom@gmail.com>
2022-01-19 15:10:05 +0100

    colors data added  sql

---
+++ database/color_table.php
@@ -0,0 +1,96 @@
+<?php
+
+function seedTable()
+{
+
+
+    $colors = [
+        [
+            'id'    => "1",
+            'name'  => "RAL 3005",
+            'bGC'   => "#59191f"
+        ],
+
+        [
+            'id'    => "2",
+            'name'  => "RAL 5004",
+            'bGC'   => "#2C3437"
+        ],
+
+        [
+            'id'    => "3",
+            'name'  => "RAL 5011",
+            'bGC'   => "#16293A"
+        ],
+
+        [
+            'id'    => "4",
+            'name'  => "RAL 6009",
+            'bGC'   => "#26332D"
+        ],
+
+        [
+            'id'    => "5",
+            'name'  => "RAL 6064",
+            'bGC'   => "#272d2d"
+        ],
+
+        [
+            'id'    => "6",
+            'name'  => "RAL 7016",
+            'bGC'   => "#383e42"
+        ],
+
+        [
+            'id'    => "7",
+            'name'  => "RAL 7039",
+            'bGC'   => "#6b665e"
+        ],
+
+        [
+            'id'    => "8",
+            'name'  => "RAL 8014",
+            'bGC'   => "#4a3526"
+        ],
+
+        [
+            'id'    => "9",
+            'name'  => "RAL 8022",
+            'bGC'   => "#1a1718"
+        ],
+
+        [
+            'id'    => "10",
+            'name'  => "RAL 9001",
+            'bGC'   => "#e9e0d2"
+        ],
+
+        [
+            'id'    => "11",
+            'name'  => "RAL 9006",
+            'bGC'  => "#a1a1a0"
+        ],
+
+        [
+            'id'    => "12",
+            'name'  => "RAL 9019",
+            'bGC'   => "#fcfaf5"
+        ],
+
+        [
+            'id'    => "13",
+            'name'  => "RAL 9016",
+            'bGC'   => "#edede6"
+        ]
+    ];
+
+    $sql = "TRUNCATE `screens`.`colors`";
+
+    query($sql);
+
+    foreach ($colors as $color) {
+
+
+        insert($color, 'colors');
+    }
+}
